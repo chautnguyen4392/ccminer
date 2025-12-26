7b89a828323f437d8679964f48eb3faea5c56ed8
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2025-12-26 10:37:02 +0700

    Use __shfl_sync instead of __shfl

--- scrypt/nv_kernel2.cu
+++ scrypt/nv_kernel2.cu
@@ -20,6 +20,13 @@
 
 #define THREADS_PER_WU 1  // single thread per hash
 
+// Use synchronized shuffle for CUDA 9.0+ (required for Volta/Ampere and later)
+#if CUDA_VERSION >= 9000 && __CUDA_ARCH__ >= 300
+#define SHFL(var, srcLane, width) __shfl_sync(0xFFFFFFFFu, var, srcLane, width)
+#else
+#define SHFL(var, srcLane, width) __shfl(var, srcLane, width)
+#endif
+
 #if __CUDA_ARCH__ < 350
 	// Kepler (Compute 3.0)
 	#define __ldg(x) (*(x))
@@ -113,10 +120,10 @@ static __device__ uint4& operator^=(uint4& left, const uint4& right)
 __device__ __forceinline__ uint4 shfl4(const uint4 val, unsigned int lane, unsigned int width)
 {
 	return make_uint4(
-		(unsigned int)__shfl((int)val.x, lane, width),
-		(unsigned int)__shfl((int)val.y, lane, width),
-		(unsigned int)__shfl((int)val.z, lane, width),
-		(unsigned int)__shfl((int)val.w, lane, width)
+		(unsigned int)SHFL((int)val.x, lane, width),
+		(unsigned int)SHFL((int)val.y, lane, width),
+		(unsigned int)SHFL((int)val.z, lane, width),
+		(unsigned int)SHFL((int)val.w, lane, width)
 	);
 }
 
@@ -208,21 +215,21 @@ __device__ __forceinline__ void __transposed_read_BC(const uint4 *S, uint4 (&B)[
 	// read and rotate rows, in reverse row order
 	uint4 T1[8], T2[8];
 	const uint4 *loc;
-	loc = &S[(spacing*2*(32*tile   ) +  lane8      + 8*__shfl(row, 0, 8))];
+	loc = &S[(spacing*2*(32*tile   ) +  lane8      + 8*SHFL(row, 0, 8))];
 	T1[7] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+4 ) + (lane8+7)%8 + 8*__shfl(row, 1, 8))];
+	loc = &S[(spacing*2*(32*tile+4 ) + (lane8+7)%8 + 8*SHFL(row, 1, 8))];
 	T1[6] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+8 ) + (lane8+6)%8 + 8*__shfl(row, 2, 8))];
+	loc = &S[(spacing*2*(32*tile+8 ) + (lane8+6)%8 + 8*SHFL(row, 2, 8))];
 	T1[5] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+12) + (lane8+5)%8 + 8*__shfl(row, 3, 8))];
+	loc = &S[(spacing*2*(32*tile+12) + (lane8+5)%8 + 8*SHFL(row, 3, 8))];
 	T1[4] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+16) + (lane8+4)%8 + 8*__shfl(row, 4, 8))];
+	loc = &S[(spacing*2*(32*tile+16) + (lane8+4)%8 + 8*SHFL(row, 4, 8))];
 	T1[3] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+20) + (lane8+3)%8 + 8*__shfl(row, 5, 8))];
+	loc = &S[(spacing*2*(32*tile+20) + (lane8+3)%8 + 8*SHFL(row, 5, 8))];
 	T1[2] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+24) + (lane8+2)%8 + 8*__shfl(row, 6, 8))];
+	loc = &S[(spacing*2*(32*tile+24) + (lane8+2)%8 + 8*SHFL(row, 6, 8))];
 	T1[1] = __ldg(loc);
-	loc = &S[(spacing*2*(32*tile+28) + (lane8+1)%8 + 8*__shfl(row, 7, 8))];
+	loc = &S[(spacing*2*(32*tile+28) + (lane8+1)%8 + 8*SHFL(row, 7, 8))];
 	T1[0] = __ldg(loc);
 
 	// rotate columns down using a barrel shifter simulation
